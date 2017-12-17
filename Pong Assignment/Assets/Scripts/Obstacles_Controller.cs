01fe38bd8effbb83096f7a82f52084cf78781964
NathanielB16 <32700278+NathanielB16@users.noreply.github.com>
2017-12-17 17:41:53 +0100

    1.7
    Making the obstacles of Level_03 moving up and down and making them solid.

---
+++ Pong Assignment/Assets/Scripts/Obstacles_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles_Controller : MonoBehaviour {
+
+    public GameObject Obstacle;
+
+    public float Speed;
+
+    public Transform Starting_Pt;
+
+    public Transform[] points;
+
+    public int Pt_Choice;
+
+	void Start () {
+
+        Starting_Pt = points[Pt_Choice];
+		
+	}
+	
+	void Update () {
+
+        Obstacle.transform.position = Vector3.MoveTowards(Obstacle.transform.position, Starting_Pt.position, Time.deltaTime * Speed);
+
+        if (Obstacle.transform.position == Starting_Pt.position)
+        {
+
+            Pt_Choice++;
+
+            if (Pt_Choice == points.Length)
+            {
+
+                Pt_Choice = 0;
+
+            }
+
+            Starting_Pt = points[Pt_Choice]; 
+        }
+	}
+}
